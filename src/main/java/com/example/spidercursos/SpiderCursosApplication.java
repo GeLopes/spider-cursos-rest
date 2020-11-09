34e2e27916b6b243df8891b274c7cba5e3e85fc9
Géssica Ferreira <gessica.ferreira@e-unicred.com.br>
2020-11-08 22:35:32 -0300

    teste

---
+++ src/main/java/com/example/spidercursos/SpiderCursosApplication.java
@@ -0,0 +1,13 @@
+package com.example.spidercursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiderCursosApplication.class, args);
+	}
+
+}
